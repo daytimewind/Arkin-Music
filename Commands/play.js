d1460feb886ba39e46a76aa403ad1685cf59fd36
Reimu2001 <ltdat2001alt@gmail.com>
2018-08-31 09:35:44 +0700

    inital file

--- Commands/play.js
+++
@@ -1,6 +0,0 @@
-const ytdl = require('ytdl-core');
-
-exports.run = async (client, message, args, ops) => {
-    if (!message.member.voiceChannel) return message.channel.send('Please connect to a voice channel first!');
-    if (message.guild.me.voiceChannel) return message.channel.send('Sorry! The bot is already connected!')
-}
